864589bee06dad8c8aaf9061e033a673d46eb0a8
Almazbek Tynybekov <almazbek.tynybekov@timelysoft.net>
2025-04-19 18:18:59 +0600

    fix / sumDoubleTest

--- src/test/java/codingBatTest/warmup1Test/SumDoubleTest.java
+++ src/test/java/codingBatTest/warmup1Test/SumDoubleTest.java
@@ -2,13 +2,19 @@ package codingBatTest.warmup1Test;
 
 import com.digital_nomads.codingBat.warmup1.SumDouble;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SumDoubleTest {
+    SumDouble sumDouble;
+
+    @BeforeClass
+    void beforeClassTest() {
+        sumDouble = new SumDouble();
+    }
 
     @Test
     void sumDoubleTest() {
-        SumDouble sumDouble = new SumDouble();
         Assert.assertEquals(sumDouble.sumDouble(4, 4), 16);
     }
 }
